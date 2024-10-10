b0627943c01f80030e89b5dcfed6cd9c0e8dc9c3
tugi4 <131661077+tugi4@users.noreply.github.com>
2024-10-10 15:20:03 +0200

    car switch

---
+++ src/carSwitcher.js
@@ -0,0 +1,24 @@
+/*export class CarSwitcher {
+    /* constructor(imageElementId) {
+        this.imageElement = document.getElementById(imageElementId);
+        this.currentIndex = 0;
+        this.carImages = ['src/cle.png', 'src/m4Competetion.png', 'src/m4.png']; 
+    }
+
+    nextCar() {
+        this.currentIndex = (this.currentIndex + 1) % this.carImages.length;
+        console.log("Next Car Index: " + this.currentIndex);  // Debugging
+        this.updateCarImage();
+    }
+    
+    previousCar() {
+        this.currentIndex = (this.currentIndex - 1 + this.carImages.length) % this.carImages.length;
+        console.log("Previous Car Index: " + this.currentIndex);  // Debugging
+        this.updateCarImage();
+    }
+    
+
+    updateCarImage() {
+        this.imageElement.src = this.carImages[this.currentIndex];
+    } 
+}*/
